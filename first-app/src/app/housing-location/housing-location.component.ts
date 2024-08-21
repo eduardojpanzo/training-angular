8cd1be00f8393445964db35dc02b06bd48a1172d
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2024-08-21 05:30:21 +0100

    feat:Creating an interface

--- first-app/src/app/housing-location/housing-location.component.ts
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
   selector: "app-housing-location",
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: ` <p>housing-location works!</p> `,
   styleUrls: ["./housing-location.component.css"],
 })
