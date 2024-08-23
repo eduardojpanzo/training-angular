a34fb814ec8cf6afd905ee5069b391a6b17c144c
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2024-08-23 09:58:42 +0100

    feat: Add an interpolation to a component’s template

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -14,7 +14,7 @@ import { HousingLocation } from "../housinglocation";
         <button class="primary" type="button">Search</button>
       </form>
     </section>
-    <section>
+    <section class="results">
       <app-housing-location
         [housingLocation]="housingLocation"
       ></app-housing-location>
