3e5b4ab827c1a24a72db4e00063237cc13f59d6d
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2024-10-17 05:34:58 +0100

    feat: definindo o footer e o header

--- memoteca/src/app/app.component.ts
+++ memoteca/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'memoteca';
